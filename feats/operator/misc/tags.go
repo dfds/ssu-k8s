0d743c21382fd07a3408744d914b80d00dc27333
Andreas Frisch <andfris@dfds.com>
2026-03-10 14:13:39 +0100

    update allowedTags

--- feats/operator/misc/tags.go
+++ feats/operator/misc/tags.go
@@ -8,6 +8,7 @@ var AllowedTags = map[string]string{
 	"dfds.service.availability": fmt.Sprintf("%s/service-availability", labelPrefix),
 	"dfds.service.criticality":  fmt.Sprintf("%s/service-criticality", labelPrefix),
 	"dfds.data.classification":  fmt.Sprintf("%s/data-classification", labelPrefix),
+	"dfds.env":                  fmt.Sprintf("%s/env", labelPrefix),
 }
 
 func IsTagAllowed(tag string) bool {
