8c32f83f58c364769eb1b7c5221178a46da298c1
Andreas Frisch <andfris@dfds.com>
2026-03-19 10:36:37 +0100

    match tagging in k8s and ssu

--- feats/operator/misc/tags.go
+++ feats/operator/misc/tags.go
@@ -5,11 +5,11 @@ import "fmt"
 var AllowedTags = map[string]string{
 	"dfds.cost.centre": fmt.Sprintf("%s/cost-centre", labelPrefix),
 	//"dfds.owner":                fmt.Sprintf("%s/owner", labelPrefix),
-	"dfds.service.availability": fmt.Sprintf("%s/service-availability", labelPrefix),
-	"dfds.service.criticality":  fmt.Sprintf("%s/service-criticality", labelPrefix),
-	"dfds.data.classification":  fmt.Sprintf("%s/data-classification", labelPrefix),
-	"dfds.env":                  fmt.Sprintf("%s/env", labelPrefix),
-	"dfds.businessCapability":   fmt.Sprintf("%s/business-capability", labelPrefix),
+	"dfds.service.availability": fmt.Sprintf("%s.service.availability", labelPrefix),
+	"dfds.service.criticality":  fmt.Sprintf("%s.service.criticality", labelPrefix),
+	"dfds.data.classification":  fmt.Sprintf("%s.data.classification", labelPrefix),
+	"dfds.env":                  fmt.Sprintf("%s.env", labelPrefix),
+	"dfds.businessCapability":   fmt.Sprintf("%s.businessCapability", labelPrefix),
 }
 
 func IsTagAllowed(tag string) bool {
