5172f25491f432084a6ef51a05fac06bceb5c6a9
Andreas Frisch <andfris@dfds.com>
2026-03-19 10:39:23 +0100

    and match tagging policy

--- feats/operator/misc/tags.go
+++ feats/operator/misc/tags.go
@@ -3,13 +3,13 @@ package misc
 import "fmt"
 
 var AllowedTags = map[string]string{
-	"dfds.cost.centre": fmt.Sprintf("%s/cost-centre", labelPrefix),
-	//"dfds.owner":                fmt.Sprintf("%s/owner", labelPrefix),
-	"dfds.service.availability": fmt.Sprintf("%s.service.availability", labelPrefix),
-	"dfds.service.criticality":  fmt.Sprintf("%s.service.criticality", labelPrefix),
-	"dfds.data.classification":  fmt.Sprintf("%s.data.classification", labelPrefix),
-	"dfds.env":                  fmt.Sprintf("%s.env", labelPrefix),
-	"dfds.businessCapability":   fmt.Sprintf("%s.businessCapability", labelPrefix),
+	"dfds.cost.centre": fmt.Sprintf("dfds.cost.centre"),
+	//"dfds.owner":              fmt.Sprintf("%s/owner", labelPrefix),
+	"dfds.service.availability": fmt.Sprintf("dfds.service.availability"),
+	"dfds.service.criticality":  fmt.Sprintf("dfds.service.criticality"),
+	"dfds.data.classification":  fmt.Sprintf("dfds.data.classification"),
+	"dfds.env":                  fmt.Sprintf("dfds.env"),
+	"dfds.businessCapability":   fmt.Sprintf("dfds.businessCapability"),
 }
 
 func IsTagAllowed(tag string) bool {
